6bf07ffa31dcb315bc2c37cf83d1592422fc024f
putheakhem <putheakhemdeveloper@gmail.com>
2016-01-30 21:47:05 +0700

    update event_name to event_title

--- db/migrate/20160129080001_events.rb
+++ db/migrate/20160129080001_events.rb
@@ -1,7 +1,7 @@
 class Events < ActiveRecord::Migration
   def self.up
       create_table :events do |t|
-         t.column :name, :string, :limit => 32, :null => false
+         t.column :title, :string, :limit => 32, :null => false
          t.column :number_date, :integer
          t.column :title, :text
          t.column :created_at, :timestamp
